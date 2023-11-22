0903d743156c454d674b7bb52e12fe547864ef6d
임성훈 <dlatjdgns999@gmail.com>
2023-11-22 19:46:58 +0900

    feat: Add Scrollbar

--- src/routes/LandingPage/index.tsx
+++ src/routes/LandingPage/index.tsx
@@ -1,19 +1,50 @@
 import Schedule from './Schedule'
 import Map from './Map'
+import Cart from './Cart'
 
 export default function LandingPage() {
   return (
     <div style={{display: 'flex', height: '1000px'}}>
-      <div style={{flex: 1, display: 'flex', flexDirection: 'column'}}>
+      <div
+        style={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          border: '2px solid #dddddd',
+          borderRadius: '8px',
+          padding: '16px',
+          margin: '8px'
+        }}>
         <div style={{flex: 1}}>
-          {/* 왼쪽상단 */}
+          {/* 왼쪽 상단 */}
           <Schedule />
-          {/* <Memo /> */}
         </div>
-        <div style={{flex: 1}}></div>
+        <div style={{flex: 1}}>{/* 왼쪽 하단 */}</div>
       </div>
-      <div style={{flex: 1}}>
-        <Map /> {/* 오른쪽 상단 */}
+
+      <div style={{flex: 1, display: 'flex', flexDirection: 'column'}}>
+        <div
+          style={{
+            flex: 2,
+            border: '2px solid #dddddd',
+            borderRadius: '8px',
+            padding: '16px',
+            margin: '8px'
+          }}>
+          {/* 오른쪽 상단 */}
+          <Map />
+        </div>
+        <div
+          style={{
+            flex: 1,
+            border: '2px solid #dddddd',
+            borderRadius: '8px',
+            padding: '16px',
+            margin: '8px'
+          }}>
+          {/* 오른쪽 하단 (장바구니 영역 추가) */}
+          <Cart />
+        </div>
       </div>
     </div>
   )
