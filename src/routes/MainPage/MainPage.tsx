1747dc9310a1daf989dd4fccf9253715516fb100
junki1912 <wnsrl1222@naver.com>
2023-11-28 15:52:56 +0900

    refactor: Header 주석 처리

--- src/routes/MainPage/MainPage.tsx
+++ src/routes/MainPage/MainPage.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import React, { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 import "./MainPage.css";
 
 interface MainPageProps {
@@ -7,58 +7,59 @@ interface MainPageProps {
   toggleLogin: () => void;
 }
 
-const Main: React.FC<MainPageProps> = ({ isLoggedIn, toggleLogin }) => {
-  const navigate = useNavigate();
-  const [searchKeyword, setSearchKeyword] = useState("");
-  const [searchCombo, setSearchCombo] = useState("user");
-  const [showTopButton, setShowTopButton] = useState(false);
+// const Main: React.FC<MainPageProps> = ({ isLoggedIn, toggleLogin }) => {
+const Main: React.FC<MainPageProps> = () => {
+  // const navigate = useNavigate();
+  // const [searchKeyword, setSearchKeyword] = useState("");
+  // const [searchCombo, setSearchCombo] = useState("user");
+  // const [showTopButton, setShowTopButton] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 1000) {
-        // 실제 사용할 때는 크기봐서 조절해야할 듯
-        setShowTopButton(true);
-      } else {
-        setShowTopButton(false);
-      }
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY > 1000) {
+  //       // 실제 사용할 때는 크기봐서 조절해야할 듯
+  //       setShowTopButton(true);
+  //     } else {
+  //       setShowTopButton(false);
+  //     }
+  //   };
 
-    window.addEventListener("scroll", handleScroll);
+  //   window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      navigate(
-        `/SignUpPage?searchcombo=${searchCombo}&searchkeyword=${searchKeyword}`,
-        { state: { searchKeyword } }
-      );
-    }
-  };
+  // const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  //   if (event.key === "Enter") {
+  //     navigate(
+  //       `/SignUpPage?searchcombo=${searchCombo}&searchkeyword=${searchKeyword}`,
+  //       { state: { searchKeyword } }
+  //     );
+  //   }
+  // };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchKeyword(event.target.value);
-  };
+  // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setSearchKeyword(event.target.value);
+  // };
 
-  const handleComboChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSearchCombo(event.target.value);
-  };
+  // const handleComboChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  //   setSearchCombo(event.target.value);
+  // };
 
-  const getPlaceholder = () => {
-    switch (searchCombo) {
-      case "user":
-        return "여행친구를 검색하세요!";
-      case "location":
-        return "가고 싶은 곳에 대한 정보를 드리겠습니다";
-      case "schedule":
-        return "아름다운 여행 일정을 만들기 위해 참고하세요";
-    }
-  };
+  // const getPlaceholder = () => {
+  //   switch (searchCombo) {
+  //     case "user":
+  //       return "여행친구를 검색하세요!";
+  //     case "location":
+  //       return "가고 싶은 곳에 대한 정보를 드리겠습니다";
+  //     case "schedule":
+  //       return "아름다운 여행 일정을 만들기 위해 참고하세요";
+  //   }
+  // };
 
   return (
     <div className="main-container">
-      <header className="header">
+      {/* <header className="header">
         <div className="top-bar">
           <a href="/">
             <img src="/topbar-logo.png" alt="Logo" className="logo" />
@@ -89,7 +90,7 @@ const Main: React.FC<MainPageProps> = ({ isLoggedIn, toggleLogin }) => {
             )}
           </div>
         </div>
-      </header>
+      </header> */}
       <main className="content">
         <div className="section-container">
           <section className="section-div" style={{ marginRight: "30%" }}>
@@ -152,13 +153,12 @@ const Main: React.FC<MainPageProps> = ({ isLoggedIn, toggleLogin }) => {
           />
         </div>
       </main>
-      <footer className="footer">© 2023 CALLYIA-TRIP</footer>
-      <button
+      {/* <button
         type="button"
         className="top-button"
         style={{ visibility: showTopButton ? "visible" : "hidden" }} // 스크롤이 조건이상 내려가면 보이게
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-      ></button>
+      ></button> */}
     </div>
   );
 };
