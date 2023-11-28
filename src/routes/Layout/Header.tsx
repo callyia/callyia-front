6d9df3940d62279033ace4ffcad5cc852ca12ca7
junki1912 <wnsrl1222@naver.com>
2023-11-28 15:48:03 +0900

    feat: init header

---
+++ src/routes/Layout/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+      <header className="header">
+        <div className="top-bar">
+          <a href="/">
+            <img src="/topbar-logo.png" alt="Logo" className="logo" />
+          </a>
+        </div>
+      </header>
+  )
+}
