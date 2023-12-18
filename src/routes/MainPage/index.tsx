0841877951378ecbe3561599b58a313c66e44c33
junki1912 <wnsrl1222@naver.com>
2023-12-18 19:48:42 +0900

    feat: 검색 페이지 초기등록, 웹폰트 등록

--- src/routes/MainPage/index.tsx
+++ src/routes/MainPage/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import Main from './MainPage'; // Adjust this import path to the location of your Main component
-
+import Main from './MainPage';
 export default function MainPage() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
