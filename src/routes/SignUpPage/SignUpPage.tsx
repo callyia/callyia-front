3ac6951ac5b94121d30db01ecb76f82f68aab22e
JUH930610 <139191061+JUH930610@users.noreply.github.com>
2023-12-19 19:51:26 +0900

    회원가입

--- src/routes/SignUpPage/SignUpPage.tsx
+++ src/routes/SignUpPage/SignUpPage.tsx
@@ -12,69 +12,73 @@ const SignUp = () => {
   };
 
   return (
-    <div className="auth-container">
-      <div className="auth-SignUp">
-        <h2>Sign Up</h2>
+    <div className="page-container">
+      <div className="shadow SignUp-form-container">
+        <div className="SignUp-form-right-side">
+          <div className="top-logo-wrap"></div>
+          <h1>콜이야 트립</h1>
+        </div>
+        <div className="SignUp-form-left-side">
+          <div className="auth-SignUp">
+            <h2>Sign Up</h2>
+          </div>
+          <div className="SignUp-input-container">
+            <div className="SignUp-input-wrap input-id">
+              <input
+                type="email"
+                value={email}
+                style={{ backgroundColor: "white", color: "black" }}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+            <div className="SignUp-input-wrap input-password">
+              <input
+                type="password"
+                value={password}
+                style={{ backgroundColor: "white", color: "black" }}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+              <div className="SignUp-input-wrap input-password">
+                <input
+                  type="password"
+                  value={confirmPassword}
+                  style={{ backgroundColor: "white", color: "black" }}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  placeholder="Confirm Password"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="SignUp-input-choice">
+            <span id="ch1">Female</span>
+            <input type="radio" name="gender" id="rd1" value="여성" checked />
+            <span id="ch2">Male</span>
+            <input type="radio" name="gender" id="rd2" value="남성" />
+          </div>
+          <div className="SignUp-input-phone">
+            <h4>Phone</h4>
+            <select id="tel1">
+              <option value="">Select</option>
+              <option value="010">010</option>
+              <option value="011">011</option>
+              <input
+                type="text"
+                value="직접 입력해주세요"
+                placeholder="직접 입력해주세요"
+              />
+            </select>{" "}
+            - <input type="text" id="tel2" />
+            - <input type="text" id="tel3" />
+          </div>
+          <div className="SignUp-btn-wrap">
+            <button className="login-btn">
+              <h1>Sign Up</h1>
+            </button>
+          </div>
+        </div>
       </div>
-      <form>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            style={{ backgroundColor: "white", color: "black" }}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            style={{ backgroundColor: "white", color: "black" }}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            value={confirmPassword}
-            style={{ backgroundColor: "white", color: "black" }}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>
-            <span>Female</span>
-          </label>
-          <input type="radio" name="gender" id="rd1" value="여성" checked />
-          <label>Male</label>
-          <input type="radio" name="gender" id="rd2" value="남성" />
-        </div>
-
-        <div>
-          <h4>Phone</h4>
-          <select id="tel1">
-            <option value="">Select</option>
-            <option value="010">010</option>
-            <option value="011">011</option>
-            <input
-              type="text"
-              value="직접 입력해주세요"
-              placeholder="직접 입력해주세요"
-            />
-          </select>{" "}
-          - <input type="text" id="tel2" />
-          - <input type="text" id="tel3" />
-        </div>
-
-        <div>
-          <button type="button" onClick={handleSignUp} style={{ color: "red" }}>
-            Sign Up
-          </button>
-        </div>
-      </form>
     </div>
   );
 };
