4bd22e28b40de504a357191cfea0543cee00a130
junki1912 <wnsrl1222@naver.com>
2024-01-22 17:10:44 +0900

    feat: userprofilepage db

--- src/routes/UserProfilePage/dummySchedule.ts
+++
@@ -1,8 +0,0 @@
-export const dummySchedule = [
-    {  sno: 1, s_name: '부산풀코스', total_day: 3, member_email: 'ch@naver.com' },
-    {  sno: 2, s_name: '제주도여행', total_day: 7, member_email: 'jk@naver.com' },
-    {  sno: 3, s_name: '여수', total_day: 2, member_email: 'sh@naver.com' },
-    {  sno: 4, s_name: '서울여행', total_day: 4, member_email: 'sg@naver.com' },
-    {  sno: 5, s_name: '국토대장정', total_day: 25, member_email: 'jh@naver.com' },
-    {  sno: 6, s_name: '통영', total_day: 1, member_email: 'jk@naver.com' },
-];
