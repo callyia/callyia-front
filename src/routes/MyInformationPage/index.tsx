a301d0797475ed74775ec0fb2e462fc8b0156a9b
java20230712 <tlswndgus15@naver.com>
2024-02-02 18:14:11 +0900

    feat: myinformationpage 추가 main 이미지 깨짐 현상 수정 더미 이미지 생성

---
+++ src/routes/MyInformationPage/index.tsx
@@ -0,0 +1,9 @@
+import MyInformation from "./MyInformationPage";
+import "./MyInformationPage.css";
+export default function MyInformationPage() {
+  return (
+    <div className="container">
+      <MyInformation />
+    </div>
+  );
+}
